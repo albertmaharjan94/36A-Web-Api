4a12c4b76f4fa045391fe1e245fb472c8e177722
albert <albert.maharjan94@gmail.com>
2026-04-30 10:42:18 +0545

    apr 30

--- src/controllers/person.controller.ts
+++ src/controllers/person.controller.ts
@@ -1,9 +1,52 @@
 import { Request, Response } from "express";
 import { dataset } from "../models/person.model";
-
+import { ApiResponseHelper } from "../utils/apihelper.util";
+import { HttpException } from "../exceptions/http-exception";
 export class PersonController {
     async getAllPersons(req: Request, res: Response) {
-        // path function
-        return res.json(dataset);
+        // return res.json(dataset);
+        try {
+            const someVar: any = {};
+            // without custom exception
+            if(!someVar.ref){
+                // throw new Error("Reference not found"); // either 
+                // return ApiResponseHelper.error(
+                //     res, "Reference not found", 404
+                // );
+                throw new HttpException(404, "Reference not found");
+            }
+
+            someVar.ref.add("test"); // error 
+
+            return ApiResponseHelper.success(
+                res, dataset, "Persons fetched successfully", 200
+            );
+        } catch (err: Error | any | HttpException) {
+            return ApiResponseHelper.error(
+                res, "Failed to fetch persons", err.status ?? 500
+            );
+        }
+    }
+    // API consistency
+    // 1. consistent response structure
+    // 2. logic through exceptions
+    // 3. combine reponse and error handling
+    async createPerson(req: Request, res: Response) {
+        const { name, age } = req.body;
+        if(!name){
+            throw new HttpException(400, "Name is required");
+        }
+        if(!age){
+            throw new HttpException(400, "Age is required");
+        }
+        const newPerson = {
+            id: dataset.length + 1,
+            name,
+            age
+        }
+        dataset.push(newPerson);
+        return ApiResponseHelper.success(
+            res, newPerson, "Person created successfully", 201
+        );
     }
 }
