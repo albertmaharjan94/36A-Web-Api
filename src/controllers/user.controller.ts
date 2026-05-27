0d2e5d8d592ea887627f2e588df1b92bd91bec34
albert <albert.maharjan94@gmail.com>
2026-05-27 12:15:16 +0545

    may 27

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,6 @@
 import { UserService } from "../services/user.service";
 import { z } from "zod";
-import { CreateUserDTO } from "../dtos/user.dto";
+import { CreateUserDTO, LoginUserDTO } from "../dtos/user.dto";
 import { ApiResponseHelper } from "../utils/apihelper.util";
 import { Request, Response } from "express";
 const userService = new UserService();
@@ -19,7 +19,25 @@ export class UserController {
             return ApiResponseHelper.error(
                 res,
                 error.message || "Internal Server Error",
-                error.statusCode || 500
+                error.status || 500
+            );
+        }
+    }
+    
+    async loginUser(req: Request, res: Response) {
+        try{
+            const parsedData = LoginUserDTO.safeParse(req.body);
+            if (!parsedData.success) {
+                return ApiResponseHelper
+                    .error(res, z.prettifyError(parsedData.error), 400);
+            }
+            const { user, token } = await userService.loginUser(parsedData.data);
+            return ApiResponseHelper.success(res, { user, token }, "Login successful");
+        }catch (error: Error | any | unknown) {
+            return ApiResponseHelper.error(
+                res,
+                error.message || "Internal Server Error",
+                error.status || 500
             );
         }
     }
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -5,5 +5,6 @@ const userRouter = Router();
 const userController = new UserController();
 
 userRouter.post("/register", userController.createUser);
+userRouter.post("/login", userController.loginUser);
 
 export default userRouter;
